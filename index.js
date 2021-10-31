ec64bba27a6522509f72d51d973a9f9b8d816139
Naz365 <nazmul.gub.cse@gmail.com>
2021-10-31 20:30:50 +0600

    some change

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.58okt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
+
+
+
+
 console.log(uri);
 
 const client = new MongoClient(uri, {
