ce746303fc9bc1cbb064b5a71d14b8aee520ade6
Naz365 <nazmul.gub.cse@gmail.com>
2021-10-31 19:15:10 +0600

    some change

--- index.js
+++ index.js
@@ -14,10 +14,6 @@ app.use(express.json());
 app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.58okt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
-
-
 
 console.log(uri);
 
